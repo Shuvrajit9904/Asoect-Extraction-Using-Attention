94866ddc250f8fcc27830dbbdddfbc1913aebf99
Shuvrajit Mukherjee <u1131529@utah.edu>
2019-02-28 14:13:15 -0700

    Attention model:TF graph built

--- model.py
+++ model.py
@@ -19,14 +19,30 @@ embedding_vector_size = 300
 tf.reset_default_graph()
 training_inputs = tf.placeholder(shape=[None, 300, 645], 
                                          dtype=tf.float32)  
+M = tf.get_variable("M", [embedding_vector_size, embedding_vector_size])
 
 
 def attention(training_inputs, M):    
     
     ys = tf.expand_dims(tf.math.reduce_mean(training_inputs, axis=1), axis=1)
     d1 = tf.matmul(M, ys)
-    d = tf.matmul(training_inputs, d1, transpose_a=True)
+    d = tf.matmul(training_inputs, d1, transpose_a=True)    
+    a = tf.nn.softmax(d)
     
-    return tf.nn.softmax(d)
+    zs = tf.matmul(a, training_inputs, transpose_a=True, transpose_b=True)
+    
+    return zs
 
-M = tf.get_variable("M", [embedding_vector_size, embedding_vector_size])
+def reconstruct_embed(k, training_inputs, M):
+    
+    zs = attention(training_inputs[0])
+    pt = tf.nn.softmax(tf.layers.dense(zs, k))
+    T = tf.get_variable("T", [embedding_vector_size, k])
+    rs = tf.matmul(T, pt, transpose_b=True)
+    
+    return rs
+
+
+
+sample_inp = training_inputs[0]
+
